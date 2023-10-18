4b4e0a622741ba08013a9de43e9dd120e63d5322
Mudit Pathak <pathak.mudit@gmail.com>
2023-10-18 12:48:32 +0000

    Add for loop for subnets

---
+++ loadbalancer.tf
@@ -0,0 +1,58 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
+resource "aws_lb_target_group" "front" {
+  name     = "application-front"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.VPCID
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 10
+    matcher             = 200
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 3
+    unhealthy_threshold = 2
+  }
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group_attachment
+resource "aws_lb_target_group_attachment" "attach-app1" {
+  count            = length(var.instance)
+  target_group_arn = aws_lb_target_group.front.arn
+  target_id        = element(var.instance[*], count.index)
+  port             = 80
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front.arn
+  }
+}
+
+module "security_group" {
+  source      = "./modules/security_group"
+  name        = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id      = lookup(var.awsprops, "vpc")
+
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
+resource "aws_lb" "front" {
+  name               = "front"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups     = [module.security_group.id]
+  subnets            = [for subnet in var.SUBNET_ID : subnet]
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "front"
+  }
+}
